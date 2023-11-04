5a79fac298e3a7f5a4d52c2b76bcb9f1a6c21afc
JaumeJD <jgarveg163@iesmartinezm.es>
2023-11-05 00:09:33 +0100

    validando campo minas

--- Portafolio/buscaminas/forms.py
+++ Portafolio/buscaminas/forms.py
@@ -1,6 +1,19 @@
 from django import forms
+from django.forms import Form
 
 
 class Tablero(forms.Form):
     rows = forms.IntegerField(label='Filas:', min_value=1, max_value=20, initial=2, required=True)
     cols = forms.IntegerField(label='Columnas:', min_value=1, max_value=15, initial=2, required=True)
+    minas = forms.IntegerField(label='Minas:', min_value=1, initial=1, required=True)
+
+    def clean_minas(self):
+        cleaned_data = self.cleaned_data
+        rows = cleaned_data.get('rows')
+        cols = cleaned_data.get('cols')
+        minas = cleaned_data.get('minas')
+
+        if minas > (rows * cols) // 2:
+            self.add_error('minas', forms.ValidationError('ASDASDASDASDASD'))
+
+        return cleaned_data
--- Portafolio/buscaminas/views.py
+++ Portafolio/buscaminas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import Tablero
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -7,6 +9,7 @@ def inicio(request):
     return render(request, 'buscaminas/index.html', {})
 
 
+@csrf_exempt
 def crea_tablero(request):
     # Se crea el objeto vacio para el formulario.
     table_form = Tablero()
@@ -14,5 +17,14 @@ def crea_tablero(request):
     # Si se ha enviado el formulario.
     if request.method == 'POST':
         table_form_param = Tablero(request.POST)
+
+        if table_form_param.is_valid():
+            rows = int(request.POST['rows'])
+            cols = int(request.POST['cols'])
+            mines = int(request.POST['minas'])
+
+            return render(request, 'buscaminas/muestraTablero.html',
+                          {'filas': rows, 'rangoF': range(rows), 'minas': mines,
+                           'columnas': cols, 'rangoC': range(cols), 'table': table_form_param})
     # Si se pide la página por primera vez.
     return render(request, 'buscaminas/crea_tablero.html', {'form': table_form})
