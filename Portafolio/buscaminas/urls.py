8ad1d3087b3a2e289e332a100cb835436dadbf8c
JaumeJD <jgarveg163@iesmartinezm.es>
2023-11-08 11:27:15 +0100

    Actualizando repo global

--- Portafolio/buscaminas/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from buscaminas import views
-
-urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('crea_tablero', views.crea_tablero, name='crea_tablero')
-]
