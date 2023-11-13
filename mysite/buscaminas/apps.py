10219b0c4a1d8561d7c7da5735def1d19196f80f
JaumeJD <jgarveg163@iesmartinezm.es>
2023-11-13 11:01:39 +0100

    Limpieza de repositorio

--- mysite/buscaminas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BuscaminasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'buscaminas'
--- mysite/buscaminas/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class CreaTablero(forms.Form):
-    rows = forms.IntegerField(label='Filas:', min_value=1, max_value=15, required=True, initial=2)
-    cols = forms.IntegerField(label='Columnas:', min_value=1, max_value=15, required=True, initial=2)
-
